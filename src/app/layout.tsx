a99293d17519c00151b82a41538fb991ee711e88
Matheus Carvie <matheuspowerdev@gmail.com>
2024-02-07 11:23:34 -0300

    Adição dos metadata + Ajustes gerais

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,17 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "MovieAI",
   description: "Explore filmes de alta qualidade com nossas recomendações de 1 a 10, abrangendo diversos gêneros para uma experiência cinematográfica diversificada.",
+  other: {
+    author: "Matheus Carvie",
+    keywords: "Recomendação de Movies por AI, MovieAI, Projeto de recomendação de filmes do Matheus Carvie.",
+    ['revisit-after']: "7 days",
+    distribution: "global",
+    robots: "index, follow",
+    googlebot: "index, follow",
+    ['apple-mobile-web-app-capable']: "yes",
+    ['apple-mobile-web-app-status-bar-style']: "white",
+    ['format-detection']: "telephone=no"
+  }
 };
 
 export default function RootLayout({
